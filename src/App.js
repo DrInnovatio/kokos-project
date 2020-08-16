4e0d07b66fc20e5e046b6a14f1a83c6074bd13db
DrInnovatio <turing1723@gmail.com>
2020-08-16 22:48:46 +1000

    React components tested

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import First from './LandingPage'
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div>
-      <First />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    text: ""
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          onChange={ e => this.setState({ text: e.target.value }) }
+          value={ this.state.text }
+        />
+        <br />
+        <h1>{ this.state.text }</h1>
+      </div>
+    );
+  }
 }
+
 
 export default App;
